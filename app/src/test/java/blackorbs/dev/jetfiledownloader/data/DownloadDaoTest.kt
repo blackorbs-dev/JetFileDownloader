db8ca41d73a7ffe342c0ae012ba59a05d02065ce
blackorbs <blackorbs@icloud.com>
2024-09-21 18:55:33 +0100

    Edited DownloadDaoTest file and wrote unit tests for downloads database

--- app/src/test/java/blackorbs/dev/jetfiledownloader/data/DownloadDaoTest.kt
+++ app/src/test/java/blackorbs/dev/jetfiledownloader/data/DownloadDaoTest.kt
@@ -1,41 +1,27 @@
 package blackorbs.dev.jetfiledownloader.data
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.paging.testing.asSnapshot
+import androidx.compose.runtime.mutableStateOf
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
-import blackorbs.dev.jetfiledownloader.entities.Download
-import blackorbs.dev.jetfiledownloader.repository.DownloadRepository
-import blackorbs.dev.jetfiledownloader.ui.download.DownloadVM
+import blackorbs.dev.jetfiledownloader.entities.Status
 import blackorbs.dev.jetfiledownloader.util.TestUtil.testDownload
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import java.time.LocalDateTime
 
 @SmallTest
 @RunWith(RobolectricTestRunner::class)
 class DownloadDaoTest {
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var database: Database
     private lateinit var downloadDao: DownloadDao
 
-    private lateinit var repo: DownloadRepository
-    private lateinit var downloadVm: DownloadVM
-
-    private val downloads = listOf(
-        testDownload(), testDownload(), testDownload(),
-        testDownload(), testDownload(), testDownload()
-    )
+    private val download = testDownload()
 
     @Before
     fun setup(){
@@ -45,56 +31,36 @@ class DownloadDaoTest {
         ).allowMainThreadQueries().build()
 
         downloadDao = database.downloadDao()
-
-//        repo = DownloadRepository(downloadDao)
-        downloadVm = DownloadVM(repo)
     }
 
     @Test
-    fun `add item to paged list test`() = runTest{
-        downloads.forEach { downloadDao.add(it) }
-
-//        val pager = TestPager(
-//            config = PagingConfig(
-//                pageSize = 2,
-//                initialLoadSize = 2,
-//                enablePlaceholders = false
-//            ),
-////            pagingSource = downloadDao.getAll()
-//        )
-
-//        assertEquals(
-//            downloads.subList(0,2).map { it.id },
-//            (pager.refresh() as Page).data.map { it.id }
-//        )
-//
-//        assertEquals(
-//            downloads.subList(2,4).map { it.id },
-//            (pager.append() as Page).data.map { it.id }
-//        )
+    fun `add and get download item to database`() = runTest{
+        assertEquals(true, downloadDao.getAll(0).isEmpty())
+        val id = downloadDao.add(download)
+        assertEquals(1, downloadDao.getAll(0).size)
+        val d = downloadDao.get(id)
+        assertEquals(download.url, d.url)
+        assertEquals(download.fileName, d.fileName)
+        assertEquals(download.filePath, d.filePath)
+        downloadDao.add(d.apply {
+            url = "kefblog.com"
+        })
+        assertEquals(1, downloadDao.getAll(0).size)
+        assertEquals("kefblog.com", downloadDao.get(id).url)
     }
 
     @Test
-    fun `get paged items test`() = runBlocking{
-        downloadDao.add(
-            Download("google.com", "test.pdf", 1200)
-                .apply {
-                    dateTime = LocalDateTime.now()
-                    filePath = "downloads/test.pdf"
-                }
-        )
-//        val pager = TestPager(
-//            config = PagingConfig(10),
-//            pagingSource = downloadDao.getAll()
-//        )
-//        assertEquals(
-//            1,
-//            (pager.refresh() as Page).data.size
-//        )
-        assertEquals(
-            1,
-            downloadVm.downloads.asSnapshot().size
-        )
+    fun `update download size and status and delete success`() = runTest {
+        assertEquals(true, downloadDao.getAll(0).isEmpty())
+        val id = downloadDao.add(download)
+        downloadDao.update(mutableStateOf(Status.Success), id)
+        downloadDao.update(12000, id)
+        assertEquals(1, downloadDao.getAll(0).size)
+        assertEquals(Status.Success, downloadDao.get(id).status.value)
+        assertEquals(12000, downloadDao.get(id).currentSize)
+
+        downloadDao.delete(downloadDao.get(id))
+        assertEquals(true, downloadDao.getAll(0).isEmpty())
     }
 
     @After
